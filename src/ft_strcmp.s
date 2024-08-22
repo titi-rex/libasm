d434f807170288bcb4487bc5e15551adcf0c05fc
reina <13titirex@pm.me>
2024-08-22 23:52:45 +0200

    wip strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -3,4 +3,26 @@ GLOBAL  ft_strcmp
 SECTION .text
 
 ft_strcmp:
-    ret
+    xor     rax, rax
+    xor     rcx, rcx
+
+
+.loop:
+        mov     al, [rdi]
+        mov     cl, [rsi]
+        ; sub     al, [rsi]
+        cmp     al, BYTE[rsi]
+        ; je      .end
+        ; cmp     al, 0
+        jne     .end
+        test     al, al
+        jz      .end
+        inc     rdi
+        inc     rsi
+        je     .loop
+
+
+.end:
+        sub     rax, rcx
+        ret
+
--- test/strcmp.c
+++ test/strcmp.c
@@ -3,20 +3,23 @@
 void _strcmp_wrapper(const char * a, const char * b)
 {
     check(ft_strcmp(a, b) == strcmp(a, b));
+    printf("exp: %d, got: %d\n", strcmp(a, b), ft_strcmp(a, b));
 }
 
 void    strcmp_tester(void)
 {
     printf("strcmp: ");
-    _strcmp_wrapper("hehe", "hehe");
-    _strcmp_wrapper("hehe", "heheeeeee");
     _strcmp_wrapper("hehe", "455heheeeeee");
-    _strcmp_wrapper("hehe", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
-    _strcmp_wrapper("hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
-    _strcmp_wrapper("hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus874a8wysiyr 3i7yslry3iyurslie", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
-    _strcmp_wrapper("", "");
-    _strcmp_wrapper("", "");
     _strcmp_wrapper("sasd", "");
+    _strcmp_wrapper("z", "");
+    _strcmp_wrapper("hehe", "hehe");
+    _strcmp_wrapper("hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
+    _strcmp_wrapper("", "");
+    _strcmp_wrapper("", "z");
     _strcmp_wrapper("", "daww");
+    _strcmp_wrapper("0", "z");
+    _strcmp_wrapper("hehe", "heheeeeee");
+    _strcmp_wrapper("hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus874a8wysiyr 3i7yslry3iyurslie", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
+    _strcmp_wrapper("hehe", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
     printf("\n");
 }

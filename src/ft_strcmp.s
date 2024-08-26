0de4884766af85289673de0ad33b9faeed8f1181
titi-rex <reina@pearl.here>
2024-08-26 14:32:39 +0200

    FIX : strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -2,27 +2,30 @@ GLOBAL  ft_strcmp
 
 SECTION .text
 
-ft_strcmp:
-    xor     rax, rax
-    xor     rcx, rcx
 
+
+ft_strcmp:
+        xor     rax, rax
+        xor     rbx, rbx
+ 
+        dec rdi
+        dec rsi
 
 .loop:
-        mov     al, [rdi]
-        mov     cl, [rsi]
-        ; sub     al, [rsi]
-        cmp     al, BYTE[rsi]
-        ; je      .end
-        ; cmp     al, 0
-        jne     .end
-        test     al, al
-        jz      .end
         inc     rdi
         inc     rsi
-        je     .loop
+        mov     al, BYTE[rdi]
 
+        test    al, al          ; test *a, '\0'
+        jz      .end            ; if *a == '\0' goto .end
 
+        cmp     al, BYTE[rsi]   ; test *a, *b
+        jz      .loop           ; if *a == *b goto .loop
+
+ 
 .end:
-        sub     rax, rcx
+        mov     bl, BYTE[rsi]
+        sub     rax, rbx
         ret
-
+ 
+ 
--- test/strcmp.c
+++ test/strcmp.c
@@ -3,7 +3,6 @@
 void _strcmp_wrapper(const char * a, const char * b)
 {
     check(ft_strcmp(a, b) == strcmp(a, b));
-    printf("exp: %d, got: %d\n", strcmp(a, b), ft_strcmp(a, b));
 }
 
 void    strcmp_tester(void)
@@ -17,9 +16,11 @@ void    strcmp_tester(void)
     _strcmp_wrapper("", "");
     _strcmp_wrapper("", "z");
     _strcmp_wrapper("", "daww");
+    _strcmp_wrapper("a", "adaww");
     _strcmp_wrapper("0", "z");
     _strcmp_wrapper("hehe", "heheeeeee");
     _strcmp_wrapper("hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus874a8wysiyr 3i7yslry3iyurslie", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
     _strcmp_wrapper("hehe", "hehslkfj sljfojes;ofh sf6s84efs4efsf jse9w73 os;l9es9rf9fru3u8sl38ursl83rus9w3l8ry sl3yr7sylwsi3rysiyr 3i7yslry3iyurslie");
     printf("\n");
 }
+
